02b89d40640c67a06892174c5220bec5029a4dd4
Breno Laonte <devlaontemoreira@gmail.com>
2022-01-16 20:39:59 -0300

    pasta apagada

--- braian/fase/javascript/carlos_fase 1.js
+++ braian/fase/javascript/carlos_fase 1.js
@@ -2,7 +2,7 @@
     
     function opcao() {
   var escolha = Number(prompt(
-      "Faça a sua escolha    \n 1 - vai sair de casa  \n 2 -  ir no vizinho \n  3 - sair pela escada de emergência."
+      "Faça a sua escolha    \n 1 - dscobrir qual é o carro  \n 2 -  largar a chave e ir embora \n  3 - se entregar ao seu destino."
   ));
 
   while (escolha != 1 || escolha != 2 || escolha != 3)
@@ -10,19 +10,19 @@
   {
 
       if (escolha == 1) {
-          window.location.href = "fase_4_escolha_1.html"
+          window.location.href = "fase_4_escolha_2.html"
           
           break;
 
 
       } else if (escolha == 2) {
-          window.location.href = "fase_4_escolha_2.html"
+          window.location.href = "game_over.html"
           break;
 
 
 
       } else if (escolha == 3) {
-          window.location.href = "fase_4_escolha_3.html"
+          window.location.href = "game_over.html"
 
 
           break;
